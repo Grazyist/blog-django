c57ad55dd83d3b758e5713f2316b215a55fe0aad
Grazyist <15103858309@163.com>
2020-05-18 22:47:11 +0800

    '数据库配置'

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -75,11 +75,14 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'blog',
+        'USER': 'itblog',
+        'PASSWORD': 'cheng1130',
+        'HOST': '127.0.0.1',
+        'PORT': '3306'
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
